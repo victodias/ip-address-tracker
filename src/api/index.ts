cb738ab0ec9f27bfddbace212a128ea98a9e03e5
Victor Dias <diasdev23@gmail.com>
2021-05-22 01:42:42 -0300

    feat: config api routes and env variables

---
+++ src/api/index.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import { LocationResponse } from 'types'
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_IPIFY_URL
+})
+
+const apiKey = process.env.REACT_APP_IPIFY_KEY
+
+export const getIpAddress = (ipAddress: string) =>
+  instance.get<LocationResponse>(
+    `/api/v1?apiKey=${apiKey}&ipAddress=${ipAddress}`
+  )
