edbc6b5e484c715924c60073a2dd5739fa85e005
Victor Dias <diasdev23@gmail.com>
2021-05-22 01:40:02 -0300

    fix: refactor names of context location

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Route, BrowserRouter as Router } from 'react-router-dom'
-import Main from 'components/Main'
-import { AddressProvider } from 'context/IpAddress'
+import Main from 'pages/Main'
+import { LocationProvider } from 'context/Location'
 import { GlobalStyles } from 'styles/globals'
 
 function Routes() {
   return (
     <Router>
       <GlobalStyles />
-      <AddressProvider>
+      <LocationProvider>
         <Route exact path="/" component={Main} />
-      </AddressProvider>
+      </LocationProvider>
     </Router>
   )
 }
